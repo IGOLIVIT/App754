4ab798bc4ebb4366e1d4d96d971a05a2f09b9070
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-08-06 14:34:11 +0300

    Initial Commit

---
+++ App754/App754App.swift
@@ -0,0 +1,17 @@
+//
+//  App754App.swift
+//  App754
+//
+//  Created by IGOR on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct App754App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
