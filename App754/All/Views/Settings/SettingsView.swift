a8c6ff4647ca60dd41630af8b34b15e8739ee5b1
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-08-14 13:24:29 +0300

    main2

--- App754/All/Views/Settings/SettingsView.swift
+++ App754/All/Views/Settings/SettingsView.swift
@@ -6,10 +6,63 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        ZStack {
+            
+            Color("bg")
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Text("Settings")
+                    .foregroundColor(.white)
+                    .font(.system(size: 28, weight: .bold))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 25)
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    
+                    VStack {
+                        
+                        Button(action: {
+                            
+                            guard let url = URL(string: "https://www.termsfeed.com/live/6d723096-5c6e-4033-8170-09cbc002f8c6") else { return }
+                            
+                            UIApplication.shared.open(url)
+                            
+                        }, label: {
+                            
+                            Text("Usage Policy")
+                                .foregroundColor(Color("prim"))
+                                .font(.system(size: 17, weight: .medium))
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 50)
+                                .background(RoundedRectangle(cornerRadius: 15).fill(.white))
+                        })
+                        
+                        Button(action: {
+                            
+                            SKStoreReviewController.requestReview()
+                            
+                        }, label: {
+                            
+                            Text("Rate Us")
+                                .foregroundColor(Color("prim"))
+                                .font(.system(size: 17, weight: .medium))
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 50)
+                                .background(RoundedRectangle(cornerRadius: 15).fill(.white))
+                        })
+                    }
+                }
+                
+            }
+            .padding()
+        }
     }
 }
 
