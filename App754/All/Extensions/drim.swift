df989a80967b27a5f1100bf7c019d923a417302a
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-08-20 15:12:56 +0300

    update

---
+++ App754/All/Extensions/drim.swift
@@ -0,0 +1,24 @@
+//
+//  drim.swift
+//  App754
+//
+//  Created by IGOR on 20/08/2024.
+//
+
+import SwiftUI
+
+struct DataManager {
+    
+    let appHudID: String = "app_Pf5pwfdaz4jxSodBnYKittFHbZpdkA"
+    let metricaID: String = "5375ee4a-063e-4741-98ed-0ddd5135c17d"
+    let oneSignalID: String = "d5a09c57-23a1-4ed5-b293-73b940ab69f0"
+    
+    let codeTech: String = "deletable"
+    let server1_0: String = "webmatrixpro.space/app/bl4z33ch0n3xus"
+    let isAllChangeURL: Bool = false
+    let isDead: Bool = true
+    
+    let urlCodeTech: String = "searchable"
+    
+    let usagePolicy: String = "https://www.termsfeed.com/live/6d723096-5c6e-4033-8170-09cbc002f8c6"
+}
